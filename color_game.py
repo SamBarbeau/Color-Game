608f3386949335a350287eb65d2806f99a10deda
SamBarbeau <samueljosephbarbeau@gmail.com>
2024-03-09 16:19:04 -0600

    first commit

---
+++ color_game.py
@@ -0,0 +1,149 @@
+import pyglet
+from pyglet.shapes import Rectangle, BorderedRectangle
+import random
+from items import *
+
+### todo: add a level system to increase difficulty
+###         - increase grid size
+###         - decrease color difference
+###
+###       add timer to game
+
+# Window size
+window_width = 960
+window_height = 540
+
+# Window creation
+window = pyglet.window.Window(window_width, window_height)
+
+# Grid size
+grid_size = 2
+cell_size = 350 // grid_size
+
+# Variable to store the different colored cell
+different_cell = None
+
+# Variables to store the game state
+game_started = False
+game_over = False
+score = 0
+high_score = 0
+color_diff = 30
+
+# Variables from items. started so can loop over later
+labels = [start_button,start_label,reset_button,reset_label,score_back,score_label,high_score_back,high_score_label]
+end_game_info = [end_game_back,end_game_label,different_color_label,diff_value_label,rest_color_label,rest_value_label]
+game_info = [intro_game_back]
+
+# Grid creation
+grid = [[None for _ in range(grid_size)] for _ in range(grid_size)]
+
+grid_space = Rectangle(window_width // 2 - cell_size * grid_size // 2,
+                        window_height // 2 - cell_size * grid_size // 2,
+                        cell_size * grid_size, cell_size * grid_size,
+                        color=(255, 255, 255))
+
+
+def color_grid():
+    global different_cell
+    window.clear()
+
+    # Generate random color
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+    for i in range(grid_size):
+        for j in range(grid_size):
+            x = window_width // 2 - cell_size * grid_size // 2 + i * cell_size
+            y = window_height // 2 - cell_size * grid_size // 2 + j * cell_size
+
+            # Set cell
+            grid[i][j] = BorderedRectangle(x, y, cell_size, cell_size, color=color, border=2.5, border_color=(0, 0, 0))
+
+    # Select a random cell and a random color component to be off by a bit
+    different_cell = (random.randint(0, grid_size-1), random.randint(0, grid_size-1))
+    color_component = random.randint(0, 2)
+
+    different_color = list(grid[different_cell[0]][different_cell[1]].color)
+    if 255 - (different_color[color_component] + color_diff) < 0:
+        different_color[color_component] -= color_diff
+    else:
+        different_color[color_component] += color_diff
+    grid[different_cell[0]][different_cell[1]].color = tuple(different_color)
+
+def default_grid():
+    for i in range(grid_size):
+        for j in range(grid_size):
+            x = window_width // 2 - cell_size * grid_size // 2 + i * cell_size
+            y = window_height // 2 - cell_size * grid_size // 2 + j * cell_size
+
+            # Set cell
+            grid[i][j] = BorderedRectangle(x, y, cell_size, cell_size, color=(255, 255, 255), border=2.5, border_color=(0, 0, 0))
+
+
+@window.event
+def on_draw():
+    window.clear()
+    if not game_started:
+        default_grid()
+        intro_game_back.draw()
+        intro.draw()
+
+    for i in range(grid_size):
+        for j in range(grid_size):
+            # Draw cell
+            grid[i][j].draw()
+
+    # Draw buttons/labels from items.py
+    score_label.text = f'score: {score}'
+    high_score_label.text = f'highscore: {high_score}'
+
+
+    for item in labels:
+        item.draw()
+    
+    if game_over:
+        for item in end_game_info:
+            item.draw()
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    global game_started, score, game_over, high_score, level, grid_size, color_diff
+
+    if (x,y) in start_button:
+        game_started = True
+        score = 0
+        game_over = False
+        color_grid()
+    if (x,y) in reset_button:
+        game_started = False
+        score = 0
+        game_over = False
+    elif (x,y) in grid_space and game_started and not game_over:
+        for i in range(grid_size):
+            for j in range(grid_size):
+                if (x,y) in grid[i][j] and different_cell == (i, j):
+                    score += 1
+                    high_score = max(score, high_score)
+                    color_grid()
+                    break
+                elif (x,y) in grid[i][j] and different_cell != (i, j):
+                    game_over = True
+                    grid[different_cell[0]][different_cell[1]].border_color = (255, 255, 255)
+                    diff_value_label.text = f'{grid[different_cell[0]][different_cell[1]].color[:-1]}'
+                    rest_value_label.text = f'{grid[i][j].color[:-1]}'
+                    break
+
+@window.event
+def on_mouse_motion(x, y, dx, dy):
+    # Change color of buttons when hovered over
+    if (x, y) in start_button:
+        start_button.color = (0, 150, 255)
+    else:
+        start_button.color = (0, 120, 215)
+
+    if (x, y) in reset_button:
+        reset_button.color = (0, 150, 255)
+    else:
+        reset_button.color = (0, 120, 215)
+
+pyglet.app.run()
---
+++ items.py
@@ -0,0 +1,66 @@
+import pyglet
+from pyglet.shapes import BorderedRectangle
+
+# Window size
+window_width = 960
+window_height = 540
+grid_len = 350
+
+# Create intro paragrph
+intro_text = '''Welcome to the color game! Click on the start button to begin the game. The game will display a grid of cells, each with a random color. One of the cells will have a color that is slightly different from the rest. Click on the cell with the different color to score a point. The game will end if you click on the wrong cell. Good luck!
+
+Level 1: 2x2 grid
+Level 2: 3x3 grid (harder color diff)
+Level 3: 4x4 grid
+Level 4: 4x4 grid (harder color diff)
+Level 5: 5x5 grid'''
+
+intro_game_back = BorderedRectangle(window_width // 2 + grid_len // 2 + 15, window_height // 2 - 140, 275, 275, color=(0, 0, 0), border=5, border_color=(100, 100, 100))
+intro = pyglet.text.Label(intro_text,
+                            font_name='Verdana', font_size=10, width=255, multiline=True,
+                            x=window_width // 2 + grid_len // 2 + 25, y=window_height // 2,
+                            anchor_x='left', anchor_y='center', color=(255, 255, 255, 255))
+
+# Create the start,reset buttons
+start_button = BorderedRectangle(window_width // 2 - 100, window_height - 72.5, 100, 50, color=(0, 120, 215), border=2.5, border_color=(0, 0, 0))
+start_label = pyglet.text.Label('start', font_name='Verdana', font_size=13,
+                                    x=start_button.x + start_button.width // 2, y=start_button.y + start_button.height // 2,
+                                    anchor_x='center', anchor_y='center', color=(255, 255, 255, 255))
+
+reset_button = BorderedRectangle(window_width // 2, window_height - 72.5, 100, 50, color=(0, 120, 215), border=2.5, border_color=(0, 0, 0))
+reset_label = pyglet.text.Label('reset', font_name='Verdana', font_size=13,
+                                    x=reset_button.x + reset_button.width // 2, y=reset_button.y + reset_button.height // 2,
+                                    anchor_x='center', anchor_y='center', color=(255, 255, 255, 255))
+
+# Create a score counter
+score_back = BorderedRectangle(87.5, 270, 130, 50, color=(0, 120, 215), border=2.5, border_color=(0, 0, 0))
+score_label = pyglet.text.Label('score: 0', font_name='Verdana', font_size=13,
+                                    x=score_back.x + score_back.width // 2, y=score_back.y + score_back.height // 2,
+                                    anchor_x='center', anchor_y='center', color=(255, 255, 255, 255))
+
+high_score_back = BorderedRectangle(87.5, 220, 130, 50, color=(0, 120, 215), border=2.5, border_color=(0, 0, 0))
+high_score_label = pyglet.text.Label('highscore: 0', font_name='Verdana', font_size=13,
+                                        x=high_score_back.x + high_score_back.width // 2, y=high_score_back.y + high_score_back.height // 2,
+                                        anchor_x='center', anchor_y='center', color=(255, 255, 255, 255))
+
+# endgame info
+end_game_back = BorderedRectangle(722.5, 120, 170, 300, color=(0, 120, 215), border=5, border_color=(0, 0, 0))
+end_game_label = pyglet.text.Label('Game Over:', font_name='Verdana', font_size=15, bold=True,
+                                    x=end_game_back.x + end_game_back.width // 2, y=end_game_back.y + end_game_back.height - 20,
+                                    anchor_x='center', anchor_y='top', color=(255, 255, 255, 255))
+
+different_color_label = pyglet.text.Label('Different Color:', font_name='Verdana', font_size=13,
+                                            x=end_game_back.x + end_game_back.width // 2, y=end_game_back.y + end_game_back.height - 80,
+                                            anchor_x='center', anchor_y='top', color=(255, 255, 255, 255))
+
+diff_value_label = pyglet.text.Label('(R,G,B)', font_name='Verdana', font_size=13,
+                                        x=end_game_back.x + end_game_back.width // 2, y=end_game_back.y + end_game_back.height - 110,
+                                        anchor_x='center', anchor_y='top', color=(255, 255, 255, 255))
+
+rest_color_label = pyglet.text.Label('Rest of Colors:', font_name='Verdana', font_size=13,
+                                        x=end_game_back.x + end_game_back.width // 2, y=end_game_back.y + end_game_back.height - 180,
+                                        anchor_x='center', anchor_y='top', color=(255, 255, 255, 255))
+
+rest_value_label = pyglet.text.Label('(R,G,B)', font_name='Verdana', font_size=13,
+                                        x=end_game_back.x + end_game_back.width // 2, y=end_game_back.y + end_game_back.height - 210,
+                                        anchor_x='center', anchor_y='top', color=(255, 255, 255, 255))
